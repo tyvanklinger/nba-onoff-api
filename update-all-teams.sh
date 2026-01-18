3aee94b2909acc1e42808bb776535a79ef1b5000
tyvanklinger <tyvanklinger@gmail.com>
2026-01-18 13:50:25 -0500

    🏀 Auto-update NBA on/off data 2026-01-18

---
+++ update-all-teams.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd /Users/tyvanklinger/Documents/nba-onoff-api
+
+teams=(
+  "Atlanta Hawks"
+  "Boston Celtics"
+  "Brooklyn Nets"
+  "Charlotte Hornets"
+  "Chicago Bulls"
+  "Cleveland Cavaliers"
+  "Dallas Mavericks"
+  "Denver Nuggets"
+  "Detroit Pistons"
+  "Golden State Warriors"
+  "Houston Rockets"
+  "Indiana Pacers"
+  "Los Angeles Clippers"
+  "Los Angeles Lakers"
+  "Memphis Grizzlies"
+  "Miami Heat"
+  "Milwaukee Bucks"
+  "Minnesota Timberwolves"
+  "New Orleans Pelicans"
+  "New York Knicks"
+  "Oklahoma City Thunder"
+  "Orlando Magic"
+  "Philadelphia 76ers"
+  "Phoenix Suns"
+  "Portland Trail Blazers"
+  "Sacramento Kings"
+  "San Antonio Spurs"
+  "Toronto Raptors"
+  "Utah Jazz"
+  "Washington Wizards"
+)
+
+for team in "${teams[@]}"; do
+  echo "Updating $team..."
+  python3 "python/${team} 2025-2026.py" --update
+  sleep 2
+done
+
+git add -A
+git commit -m "🏀 Auto-update NBA on/off data $(date +'%Y-%m-%d')"
+git push
